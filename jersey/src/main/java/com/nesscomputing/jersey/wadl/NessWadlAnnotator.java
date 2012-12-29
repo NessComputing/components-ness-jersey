857164aedc2f3d83518ccfac6376f5f88dda1caf
Steven Schlansker <steven@likeness.com>
2012-12-28 17:31:11 -0800

    Deprecate the poorly-named @RequiresAuthentication and now use @PublicApi

--- jersey/src/main/java/com/nesscomputing/jersey/wadl/NessWadlAnnotator.java
+++ jersey/src/main/java/com/nesscomputing/jersey/wadl/NessWadlAnnotator.java
@@ -15,30 +15,96 @@
  */
 package com.nesscomputing.jersey.wadl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import javax.ws.rs.ext.Provider;
 import javax.xml.namespace.QName;
 
+import com.google.common.base.Throwables;
 import com.sun.jersey.api.model.AbstractResource;
 import com.sun.jersey.api.model.AbstractResourceMethod;
 import com.sun.research.ws.wadl.Method;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.nesscomputing.logging.Log;
 
 /**
  * Ness-specific extensions to the Jersey-generated WADL
  */
 @Provider
 public class NessWadlAnnotator extends DelegatingWadlGenerator {
+    private static final Log LOG = Log.findLog();
+
     public static final String NESSAPI_XML_NS = "http://nessapi.net/xml/wadl";
+    public static final String NESSAPI_XML_PREFIX = "ness";
 
     @Override
     public Method createMethod(AbstractResource r, AbstractResourceMethod m) {
         Method method = delegate.createMethod(r, m);
-        RequiresAuthentication annotation = m.getMethod().getAnnotation(RequiresAuthentication.class);
-        if (annotation == null) {
-            annotation = r.getResourceClass().getAnnotation(RequiresAuthentication.class);
+
+        Map<Class<? extends Annotation>, Annotation> annotations = new HashMap<>();
+
+        for (Annotation a : r.getResourceClass().getAnnotations()) {
+            Class<? extends Annotation> type = a.annotationType();
+            if (type.getAnnotation(WadlAnnotation.class) != null) {
+                annotations.put(type, a);
+            }
         }
-        if (annotation != null) {
-            method.getOtherAttributes().put(new QName(NESSAPI_XML_NS, "authentication", "ness"), annotation.value().toString());
+        for (Annotation a : m.getMethod().getAnnotations()) {
+            Class<? extends Annotation> type = a.annotationType();
+            if (type.getAnnotation(WadlAnnotation.class) != null) {
+                annotations.put(type, a);
+            }
         }
+
+        for (Annotation a : annotations.values()) {
+            WadlAnnotation wadlAnnotation = checkNotNull(a.annotationType().getAnnotation(WadlAnnotation.class));
+            Map<QName, String> attr = method.getOtherAttributes();
+            String value = annotationStringValue(a);
+
+            for (String xmlElement : wadlAnnotation.value()) {
+                attr.put(new QName(NESSAPI_XML_NS, xmlElement, NESSAPI_XML_PREFIX), value);
+            }
+        }
+
         return method;
+    }
+
+    /**
+     * Return the annotation's <code>value()</code> as a String if present, else
+     * the literal "true".
+     */
+    private String annotationStringValue(Annotation a)
+    {
+        java.lang.reflect.Method valueMethod = null;
+        try {
+            valueMethod = a.annotationType().getMethod("value", new Class[0]);
+        } catch (NoSuchMethodException e) {
+            LOG.trace("No value method on %s, just using \"true\"", a.annotationType());
+        }
+
+        if (valueMethod != null)
+        {
+            Object result;
+            try {
+                result = valueMethod.invoke(a);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw Throwables.propagate(e);
+            }
+
+            if (result.getClass().isArray()) {
+                return ArrayUtils.toString(result);
+            }
+            return Objects.toString(result);
+        }
+
+        return "true";
     }
 }
